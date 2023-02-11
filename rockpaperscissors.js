0a053f0fc670d6697bb058fdb03fd385eb27c2ea
Blake Tolliver <blaketolliver87@gmail.com>
2023-02-10 21:48:57 -0500

    add score variable before loop score = 0
    Next need to determine how to increment score for win, and decrement
    score for loss. Then compare score to pos/neg/same to determine win

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -3,44 +3,54 @@
 //assign int to rock/paper/scissors string
 //log choice to console
 
-
-
-
 //Play a round of rock/paper/scissors against the computer
 //get player input for choice of rock/paper/scissors
 //compare player input vs computer random choice
 //determine winner vs loser
 //output result
 
-
+//Track result of round to determine winner at end of 5 rounds
+//loop the function 5 times
+//save the outcome at the end of each loop
+//count the outcome for human
+//count the outcome for computer
+//compare count of human vs computer
+//declare winner
+let score = 0;
 function playRound(){
     let computerChoice = Math.floor(Math.random() * 3)+1;
   function getComputerChoice(){
       if (computerChoice === 1){
-          console.log("Rock")
+          return("Rock")
       }else if (computerChoice === 2){
-          console.log("Paper")
+          return("Paper")
       }else if (computerChoice === 3){
-          console.log("Scissors")
+          return("Scissors")
       }else{
-          console.log("error")
+          return("error")
       }
-  }
-  
+    }
+
     let playerChoice = prompt("Rock, Paper, or Scissors?");
     let playerChoiceLower = playerChoice.toLowerCase();
       if (playerChoiceLower == "rock" && computerChoice == 2){
-          return("You lose! Paper beats Rock!")
+          return("You lose! Paper beats Rock!");
+          let score = score--
       }else if(playerChoiceLower == "scissors" && computerChoice == 1){
           return("You lose! Rock beats Scissors!")
+          let score = score--
       }else if(playerChoiceLower == "paper" && computerChoice == 3){
-          return("You lose! Scissors beats Paper!")
+          return("You lose! Scissors beats Paper!");
+          let score = score--;
       }else if(playerChoiceLower == "rock" && computerChoice == 3){
-          return("You Win!")
+          return("You Win!");
+          let score = score++;
       }else if(playerChoiceLower == "paper" && computerChoice == 1){
           return("You Win!")
+          let score = score++;
       }else if(playerChoiceLower == "scissors" && computerChoice ==2){
-          return("You Win!")
+          return("You Win!");
+          let score = score++;
       }else{
           return("Stalemate!")
       }
@@ -48,4 +58,14 @@ function playRound(){
   
     for (i=0; i<5; i++) {
       console.log(playRound())
+    }
+
+    if (score == Math.sign(-1)){
+        console.log("sorry, you lose!")
+    }else if(score == Math.sign(1)){
+        console.log("you win!")
+    }else if(score == score){
+        console.log("it's a tie!")
+    }else{
+        console.log(error)
     }
